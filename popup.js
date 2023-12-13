691ab9fb9cd7d1e616d2dc7e894d5dacafaeaf6f
Abhishek Manhas <147427209+quarktetra23@users.noreply.github.com>
2023-12-13 07:19:46 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,40 +1,12 @@
-// popup.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Get references to HTML elements
-    const inputElement = document.getElementById("input");
-    const convertButton = document.getElementById("convertButton");
-    const outputElement = document.getElementById("output");
-  
-    // Add a click event listener to the Convert button
-    convertButton.addEventListener("click", function () {
-      // Get the text entered by the user in the textarea
-      const text = inputElement.value;
-  
-      // Call the convertToLatex function to perform the conversion
-      const latexText = convertToLatex(text);
-  
-      // Display the LaTeX output in the outputElement
-      outputElement.textContent = latexText;
+// In your popup's JavaScript
+document.getElementById("activateButton").addEventListener("click", function() {
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        browser.tabs.sendMessage(tabs[0].id, {action: "activateCrosshair"});
     });
-  });
-  
-  function convertToLatex(text) {
-    // Replace common special characters with LaTeX equivalents
-    text = text.replace(/&/g, "\\&")
-               .replace(/%/g, "\\%")
-               .replace(/\$/g, "\\$")
-               .replace(/#/g, "\\#")
-               .replace(/_/g, "\\_")
-               .replace(/\^/g, "\\^");
-  
-    // Replace common mathematical symbols with LaTeX equivalents
-    text = text.replace(/<=/g, "\\leq")
-               .replace(/>=/g, "\\geq")
-               .replace(/!=/g, "\\neq")
-               .replace(/->/g, "\\rightarrow")
-               .replace(/<->/g, "\\leftrightarrow");
-  
-    // Surround the text with \text{} to indicate it as plain text
-    return "\\text{" + text + "}";
-  }
-  
+});
+
+document.getElementById("deactivateButton").addEventListener("click", function() {
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        browser.tabs.sendMessage(tabs[0].id, {action: "deactivateCrosshair"});
+    });
+});
